bf21d472d4c96a4b9a495f32a964151e1bff874f
Serdar Ergün <generalgord@gmail.com>
2022-11-13 19:38:10 +0300

    Created EventBus.Base

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/SubscriptionInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace EventBus.Base
+{
+    public class SubscriptionInfo
+    {
+        public Type HandleType { get; }
+
+        public SubscriptionInfo(Type handleType)
+        {
+            HandleType = handleType ?? throw new ArgumentNullException(nameof(handleType));
+        }
+
+        // Dışarıdan statik olarak SubscriptionInfo çağırarak HandlerType oluşturabilmek için.
+        public static SubscriptionInfo Typed(Type handlerType)
+        {
+            return new SubscriptionInfo(handlerType);
+        }
+    }
+}
